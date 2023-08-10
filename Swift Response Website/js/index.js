7f7cf21f1e697fbc7f7312c23240f58fff9f2045
newrocker2468 <newrocker2468@gmail.com>
2023-08-10 22:57:02 +0530

    made navbar Responsive alomost now just need to profile logo and little adjustments then we can update every page

--- Swift Response Website/js/index.js
+++ Swift Response Website/js/index.js
@@ -11,3 +11,34 @@ window.addEventListener("load", function () {
     content.classList.remove("hidden");
   });
 });
+
+/* const primarynav = document.querySelector("#primary-nav");
+const navtoggle = document.querySelector(".btn-i");
+const list = document.querySelector(".list");
+const width = window.innerWidth;
+
+navtoggle.addEventListener("click", () => {
+  if (list.style.transform === "translateX(100%)") {
+    primarynav.style.transition = "opacity 800ms, transform 600ms, delay 500ms";
+    primarynav.style.opacity = 1;
+    list.style.transform = "translateX(0)";
+  } else {
+    primarynav.style.transition = "opacity 800ms, transform 600ms, delay 500ms";
+    primarynav.style.opacity = 1;
+
+    list.style.transform = "translateX(100%)";
+  }
+}); */
+const primarynav = document.querySelector("#primary-nav");
+const navtoggle = document.querySelector(".btn-i");
+const nav = document.querySelector("#primary-nav");
+navtoggle.addEventListener("click", function () {
+  if (nav.classList.contains("visible")) {
+    /*     primarynav.style.transition = "opacity 800ms, transform 600ms, delay 500ms"; */
+
+    nav.classList.remove("visible");
+  } else {
+    nav.classList.add("visible");
+    /*     primarynav.style.transition = "opacity 800ms, transform 600ms, delay 500ms"; */
+  }
+});
